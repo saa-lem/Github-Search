4fd64a0d46ff51adf73fecd4b4794de78b3486ef
Salem Owino <salemowino18@gmail.com>
2020-04-10 18:50:50 +0300

    This commit should have new components made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitsearchFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
