7be74dfb6bb1685e98e09b9f90534c2b30fb96e4
Salem Owino <salemowino18@gmail.com>
2020-04-10 19:15:29 +0300

    Few insertions made

--- src/app/gitsearch-form/gitsearch-form.component.ts
+++ src/app/gitsearch-form/gitsearch-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-gitsearch-form',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gitsearch-form.component.css']
 })
 export class GitsearchFormComponent implements OnInit {
+
+  @Output() emitUser = new EventEmitter<any>()
+
+  userName:string;
+
+  user(){
+    this.emitUser.emit(this.userName);
+  }
 
   constructor() { }
 
