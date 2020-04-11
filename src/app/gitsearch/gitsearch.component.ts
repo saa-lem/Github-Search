846b63efd50c3b570bbe2ff52a20051390414c3b
Salem Owino <salemowino18@gmail.com>
2020-04-11 11:01:17 +0300

    this commit has imported component

--- src/app/gitsearch/gitsearch.component.ts
+++ src/app/gitsearch/gitsearch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-gitsearch',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GitsearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private kay:ServiceService) { }
 
-  ngOnInit(): void {
+  ngOnInit():void{
   }
 
 }
